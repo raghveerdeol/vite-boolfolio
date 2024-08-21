2cfc988dd2b26f67fd1ffeea1d0084c80d5cd912
raghveerdeol <deolraghveer5@gmail.com>
2024-08-21 16:13:38 +0200

    aggiunta api e stampati titoli progetto in pagina

--- src/components/AppMain.vue
+++ src/components/AppMain.vue
@@ -1,17 +1,40 @@
 <script>
+import axios from 'axios';
+
 export default {
 data() {
 return {
-
+    projects: [],
 }
+},
+methods:{
+    getProjects(){
+        axios.get('http://127.0.0.1:8000/api/projects', {
+            params: {
+
+            }
+        })
+        .then((response) => {
+            console.log(response.data.results.data);
+            this.projects = response.data.results.data;
+        })
+        .catch(function (error){
+            console.log(error);
+        })
+    }
+},
+created(){
+    this.getProjects();
 }
 }
 </script>
 
 <template>
-    <h1>
-        Main
-    </h1>
+    <ul>
+        <li v-for="project in projects">
+            {{ project.title }}
+        </li> 
+    </ul>
 </template>
 
 <style scoped>
